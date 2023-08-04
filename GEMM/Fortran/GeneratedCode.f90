8fee27d7543de4867c30cd61ecd9418d3da73ce9
alexishuante <140995548+alexishuante@users.noreply.github.com>
2023-08-03 22:47:09 -0700

    Update GeneratedCode.f90

--- GEMM/Fortran/GeneratedCode.f90
+++ GEMM/Fortran/GeneratedCode.f90
@@ -1,163 +1,197 @@
 ! OpenMP
 
 ! Give me code for the following:
-! Fortran, OpenMP, General matrix-vector multiply, subroutine
+! Fortran, OpenMP, General matrix-matrix multiply, subroutine
 
-Program MatrixVectorMultiply
-  Use, Intrinsic :: ISO_C_BINDING, Only : C_INT
-  Implicit None
+Program MatrixMultiply
+  Use ISO_C_BINDING
+  Implicit none
 
   ! Declare variables
-  Real(C_FLOAT), Allocatable :: A(:, :)
-  Real(C_FLOAT), Allocatable :: B(:)
-  Real(C_FLOAT), Allocatable :: C(:)
-  Integer :: i, j, n
-  Logical :: thread_created = .false.
+  Real(C_DOUBLE), allocatable :: A(:, :)
+  Real(C_DOUBLE), allocatable :: B(:, :)
+  Real(C_DOUBLE), allocatable :: C(:, :)
+  integer :: ierr
+  integer :: n = size(A, dimension=1)
+  integer :: m = size(A, dimension=2)
+  integer :: p = size(B, dimension=1)
+  integer :: q = size(B, dimension=2)
+  integer :: r = size(C, dimension=1)
+  integer :: s = size(C, dimension=2)
 
-  ! Set up input parameters
-  n = size(A, dimension=1)
-  allocate(A(n, n))
-  allocate(B(n))
-  allocate(C(n))
+  ! Allocate memory for matrices
+  allocate(A(n,m))
+  allocate(B(p,q))
+  allocate(C(r,s))
 
-  ! Initialize matrices and vector
-  do concurrent (i=1:n)
-    A(i,:) = reshape((/ (i-1)*10 + 1, 2*i-1, 3*i+1 /), shape(A))
-    B(i) = i**2
-  enddo
+  ! Initialize matrices
+  do concurrent (i=1:n, j=1:m) A(i,j) = 0.0d0
+  do concurrent (i=1:p, j=1:q) B(i,j) = 0.0d0
+  do concurrent (i=1:r, j=1:s) C(i,j) = 0.0d0
 
-  ! Define OpenMP parallel region
-  !$omp parallel num_threads(4) default(none) shared(A, B, C)
+  ! Define subroutine for matrix-matrix multiplication
+  subroutine mmul(A, B, C, n, m, p, q, r, s) result(D)
+    implicit none
+    real(c_double), intent(in) :: A(n, m)
+    real(c_double), intent(in) :: B(p, q)
+    real(c_double), intent(out) :: C(r, s)
+    integer :: n, m, p, q, r, s
+    !$omp parallel num_threads(4) default(none) shared(A, B, C)
+      ! Perform matrix-matrix multiplication
+      do concurrent (i=1:n, j=1:q, k=1:p) C(i,j) = C(i,j) + A(i,k)*B(k,j)
+    !$omp end parallel
+  contains
+    ! Function to perform matrix-vector multiplication
+    function dotProduct(A, B, n) result(dot)
+      implicit none
+      real(c_double), intent(in) :: A(n)
+      real(c_double), intent(in) :: B(n)
+      integer :: n
+      dot = sum(A*B)
+    end function dotProduct
+  end subroutine mmul
 
-  ! Perform matrix-vector multiplication
-  do concurrent (i=1:n)
-    do concurrent (j=1:n)
-      C(i) = C(i) + A(i,j)*B(j)
-    enddo
-  enddo
+  ! Call subroutine
+  call mmul(A, B, C, n, m, p, q, r, s)
 
-  !$omp end parallel
-
-  ! Free memory
+  ! Deallocate memory for matrices
   deallocate(A, B, C)
-end program MatrixVectorMultiply
+end program MatrixMultiply
 
-! ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+! -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ! OpenMP (offload)
 
 ! Give me code for the following:
-! Fortran, OpenMP (offload), General matrix-vector multiply, subroutine
+! Fortran, OpenMP (offload), General matrix-matrix multiply, subroutine
 
-Program MatrixVectorMultiply
-  Use, Intrinsic :: ISO_C_BINDING, Only : C_INT
-  Implicit None
+Program MatrixMultiply
+  Use ISO_C_BINDING
+  Implicit none
 
   ! Declare variables
-  Real(C_FLOAT), Allocatable :: A(:, :)
-  Real(C_FLOAT), Allocatable :: B(:)
-  Real(C_FLOAT), Allocatable :: C(:)
-  Integer :: i, j, n
-  Logical :: thread_created = .false.
+  Real(C_DOUBLE), allocatable :: A(:, :)
+  Real(C_DOUBLE), allocatable :: B(:, :)
+  Real(C_DOUBLE), allocatable :: C(:, :)
+  integer :: ierr
+  integer :: n = size(A, dimension=1)
+  integer :: m = size(A, dimension=2)
+  integer :: p = size(B, dimension=1)
+  integer :: q = size(B, dimension=2)
+  integer :: r = size(C, dimension=1)
+  integer :: s = size(C, dimension=2)
 
-  ! Set up input parameters
-  n = size(A, dimension=1)
-  allocate(A(n, n))
-  allocate(B(n))
-  allocate(C(n))
+  ! Allocate memory for matrices
+  allocate(A(n,m))
+  allocate(B(p,q))
+  allocate(C(r,s))
 
-  ! Initialize matrices and vector
-  do concurrent (i=1:n)
-    A(i,:) = reshape((/ (i-1)*10 + 1, 2*i-1, 3*i+1 /), shape(A))
-    B(i) = i**2
-  enddo
+  ! Initialize matrices
+  do concurrent (i=1:n, j=1:m) A(i,j) = 0.0d0
+  do concurrent (i=1:p, j=1:q) B(i,j) = 0.0d0
+  do concurrent (i=1:r, j=1:s) C(i,j) = 0.0d0
 
-  ! Define OpenMP parallel region with offloading
-  !$omp parallel num_threads(4) default(none) shared(A, B, C) firstprivate(n)
+  ! Define subroutine for matrix-matrix multiplication
+  subroutine mmul(A, B, C, n, m, p, q, r, s) result(D)
+    implicit none
+    real(c_double), intent(in) :: A(n, m)
+    real(c_double), intent(in) :: B(p, q)
+    real(c_double), intent(out) :: C(r, s)
+    integer :: n, m, p, q, r, s
+    !$omp declare mapper(present)
+    !$omp declare target(present)
+    !$omp teams distribute parallel do concurrent(4) default(none) shared(A, B, C)
+    !$omp end declare target
+  contains
+    ! Function to perform matrix-vector multiplication
+    function dotProduct(A, B, n) result(dot)
+      implicit none
+      real(c_double), intent(in) :: A(n)
+      real(c_double), intent(in) :: B(n)
+      integer :: n
+      dot = sum(A*B)
+    end function dotProduct
+  end subroutine mmul
 
-  ! Offload computation to accelerator
-  !$omp offload (A, B, C) proc(my_matrix_vector_multiply)
+  ! Offload computation to GPU
+  call omp_set_num_teams(4)
+  call omp_set_team_size(4)
+  call mmul(A, B, C, n, m, p, q, r, s)
 
   ! Synchronize threads
-  !$omp barrier
+  call omp_barrier()
 
-  ! Free memory
+  ! Deallocate memory for matrices
   deallocate(A, B, C)
+end program MatrixMultiply
 
-contains
-
-  subroutine my_matrix_vector_multiply(A, B, C, n) result(D)
-    Real(C_FLOAT), Intent(in) :: A(n, n), B(n)
-    Real(C_FLOAT), Intent(out) :: C(n)
-    integer :: n
-
-    ! Compute matrix-vector product
-    do concurrent (i=1:n)
-      do concurrent (j=1:n)
-        D(i) = D(i) + A(i,j)*B(j)
-      enddo
-    enddo
-
-    return
-  end subroutine my_matrix_vector_multiply
-end program MatrixVectorMultiply
-
-! ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+! -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ! OpenACC
 
 ! Give me code for the following:
-! Fortran, OpenACC, General matrix-vector multiply, subroutine
+! Fortran, OpenACC, General matrix-matrix multiply, subroutine
 
-Program MatrixVectorMultiply
-  Use, Intrinsic :: ISO_C_BINDING, Only : C_INT
-  Implicit None
+program main
+  use, intrinsic :: iso_c_binding
+  use :: acc
+  implicit none
+
+  type(acc::array) :: A, B, C
+  integer :: n, m, p, q, r, s
+  integer :: err
 
   ! Declare variables
-  Real(C_FLOAT), Allocatable :: A(:, :)
-  Real(C_FLOAT), Allocatable :: B(:)
-  Real(C_FLOAT), Allocatable :: C(:)
-  Integer :: i, j, n
-  Logical :: thread_created = .false.
+  n = size(A, dim=1)
+  m = size(A, dim=2)
+  p = size(B, dim=1)
+  q = size(B, dim=2)
+  r = size(C, dim=1)
+  s = size(C, dim=2)
 
-  ! Set up input parameters
-  n = size(A, dimension=1)
-  allocate(A(n, n))
-  allocate(B(n))
-  allocate(C(n))
+  ! Allocate memory for matrices
+  allocate(A(n, m))
+  allocate(B(p, q))
+  allocate(C(r, s))
 
-  ! Initialize matrices and vector
-  do concurrent (i=1:n)
-    A(i,:) = reshape((/ (i-1)*10 + 1, 2*i-1, 3*i+1 /), shape(A))
-    B(i) = i**2
-  enddo
+  ! Initialize matrices
+  do concurrent (i=1:n, j=1:m) A(i,j) = 0.0d0
+  do concurrent (i=1:p, j=1:q) B(i,j) = 0.0d0
+  do concurrent (i=1:r, j=1:s) C(i,j) = 0.0d0
 
-  ! Define OpenACC parallel region
-  !$acc parallel num_gangs(4) default(none) shared(A, B, C) firstprivate(n)
+  ! Define subroutine for matrix-matrix multiplication
+  subroutine mmul(A, B, C, n, m, p, q, r, s) result(D)
+    implicit none
+    real(c_double), intent(in) :: A(n, m)
+    real(c_double), intent(in) :: B(p, q)
+    real(c_double), intent(out) :: C(r, s)
+    integer :: n, m, p, q, r, s
+    !$omp declare mapper(present)
+    !$omp declare target(present)
+    !$omp teams distribute parallel do concurrent(4) default(none) shared(A, B, C)
+    !$omp end declare target
+  contains
+    ! Function to perform matrix-vector multiplication
+    function dotProduct(A, B, n) result(dot)
+      implicit none
+      real(c_double), intent(in) :: A(n)
+      real(c_double), intent(in) :: B(n)
+      integer :: n
+      dot = sum(A*B)
+    end function dotProduct
 
-  ! Offload computation to accelerator
-  !$acc loop gang worker(A, B, C, n)
+    ! Perform matrix-matrix multiplication
+    do concurrent (i=1:r, j=1:s)
+       C(i,j) = dotProduct(A(i,:), B(:,j), m)
+    end do
+  end subroutine mmul
+
+  ! Call subroutine
+  call mmul(A, B, C, n, m, p, q, r, s)
 
   ! Synchronize threads
-  !$acc synchronize
+  call omp_barrier()
 
-  ! Free memory
+  ! Deallocate memory for matrices
   deallocate(A, B, C)
-
-contains
-
-  subroutine my_matrix_vector_multiply(A, B, C, n) result(D)
-    Real(C_FLOAT), Intent(in) :: A(n, n), B(n)
-    Real(C_FLOAT), Intent(out) :: C(n)
-    integer :: n
-
-    ! Compute matrix-vector product
-    do concurrent (i=1:n)
-      do concurrent (j=1:n)
-        D(i) = D(i) + A(i,j)*B(j)
-      enddo
-    enddo
-
-    return
-  end subroutine my_matrix_vector_multiply
-end program MatrixVectorMultiply
+end program main
