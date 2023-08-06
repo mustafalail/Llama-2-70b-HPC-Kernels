9fb1caafee95e23d34ae1632b1d838d4b28464c7
alexishuante <140995548+alexishuante@users.noreply.github.com>
2023-08-05 20:50:15 -0500

    Update GeneratedCode.jl

--- GEMV/Julia/GeneratedCode.jl
+++ GEMV/Julia/GeneratedCode.jl
@@ -1,1 +1,65 @@
+# Threads
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia Threads general matrix-matrix multiply"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# CUDA
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia CUDA general matrix-matrix multiply"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# AMDGPU
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia AMDGPU general matrix-matrix multiply"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# KernelAbstractions
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia KernelAbstractions general matrix-matrix multiply"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
