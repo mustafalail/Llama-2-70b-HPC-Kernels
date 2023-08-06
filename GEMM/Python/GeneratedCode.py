94b9e56795d2d2b13852c8e247290d36dd868fc2
alexishuante <140995548+alexishuante@users.noreply.github.com>
2023-08-05 20:13:39 -0500

    Update GeneratedCode.py

--- GEMM/Python/GeneratedCode.py
+++ GEMM/Python/GeneratedCode.py
@@ -1,1 +1,56 @@
 
+# numpy
+
+# Prompt: "Create 3 code suggestions using the following parameters: Python numpy general matrix-matrix multiply def."
+
+# VERSION 1 #
+
+
+# VERSION 2 #
+
+
+# VERSION 3 #
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# cuPy
+
+# Prompt: "Create 3 code suggestions using the following parameters: Python cuPy general matrix-matrix multiply def."
+
+# VERSION 1 #
+
+
+# VERSION 2 #
+
+
+# VERSION 3 #
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# pyCUDA
+
+# Prompt: "Create 3 code suggestions using the following parameters: Python pyCUDA general matrix-matrix multiply def."
+
+# VERSION 1 #
+
+
+# VERSION 2 #
+
+
+# VERSION 3 #
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Numba
+
+# Prompt: "Create 3 code suggestions using the following parameters: Python Numba general matrix-matrix multiply def."
+
+# VERSION 1 #
+
+
+# VERSION 2 #
+
+
+# VERSION 3 #
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
