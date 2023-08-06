0a02473379d7b07c5d1128c80215c16ec3e65343
alexishuante <140995548+alexishuante@users.noreply.github.com>
2023-08-05 20:49:06 -0500

    Update GeneratedCode.jl

--- GEMM/Julia/GeneratedCode.jl
+++ GEMM/Julia/GeneratedCode.jl
@@ -1,1 +1,64 @@
+# Threads
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia Threads AXPY"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# CUDA
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia CUDA AXPY"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# AMDGPU
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia AMDGPU AXPY"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# KernelAbstractions
+
+# Prompt: "Create 3 code suggestions using the following parameters: Julia KernelAbstractions AXPY"
+
+# SUGGESTION 1 #
+
+
+# SUGGESTION 2 #
+
+
+# SUGGESTION 3 #
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
